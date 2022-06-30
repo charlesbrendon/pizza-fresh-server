422471cf03bcf7c5a4a871b0df62b1c34c63bee3
Charles Brendon <charlesbrendonss@gmail.com>
2022-06-29 23:42:26 -0300

    feat(table): implement prisma

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
+const baseAPI = 'http://localhost:3150/api';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'Server is running! 🚀\n Please check http://localhost:3150/api for Swagger docs...';
+    return `Server is running! 🚀 \n Please check:  ${baseAPI} for Swagger docs...`;
   }
 }
--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -5,7 +5,7 @@ import { TableService } from './table.service';
 @ApiTags('table')
 @Controller('table')
 export class TableController {
-  constructor(private tableService: TableService) {}
+  constructor(private readonly tableService: TableService) {}
 
   @Get()
   findAll() {
@@ -13,7 +13,7 @@ export class TableController {
   }
 
   @Post()
-  create(@Body() createTableDto: CreateTableDto) {
-    return this.tableService.create(createTableDto);
+  create(@Body() dto: CreateTableDto) {
+    return this.tableService.create(dto);
   }
 }
