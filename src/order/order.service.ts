6e8674910f6222a1524103f4896686c5eb054611
Charles Brendon <charlesbrendonss@gmail.com>
2022-07-09 12:38:58 -0300

    feat(order): implement  order creation (WIP)

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { handleError } from 'src/utils/handle-error.util';
 import { CreateOrderDto } from './dto/create-order.dto';
 
 @Injectable()
 export class OrderService {
   constructor(private readonly prisma: PrismaService) {}
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  create(dto: CreateOrderDto) {
+    const data: Prisma.OrderCreateInput = {
+      user: {
+        connect: { id: dto.userId },
+      },
+      table: {
+        connect: { number: dto.tableNumber },
+      },
+    };
+    return this.prisma.order.create({ data }).catch(handleError);
   }
 
   findAll() {
