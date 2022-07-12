226bcd6d4b551e9581f50ef08e673ea9f7882914
Charles Brendon <charlesbrendonss@gmail.com>
2022-07-12 20:56:09 -0300

    feat(auth) generate resource

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginResponseDto } from './dto/login-response.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+@ApiTags('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'Realizar login, recebendo um token de autenticação',
+  })
+  async login(@Body() loginDto: LoginDto): Promise<LoginResponseDto> {
+    return {
+      token: 'Test',
+      user: undefined,
+    };
+  }
+}
