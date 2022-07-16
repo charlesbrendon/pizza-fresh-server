73dbcc54dcdb9092e882c165d1e34fb64be8c253
Charles Brendon <charlesbrendonss@gmail.com>
2022-07-16 00:23:58 -0300

    feat(auth): implement login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginResponseDto } from './dto/login-response.dto';
@@ -6,7 +7,10 @@ import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prima: PrismaService) {}
+  constructor(
+    private readonly prima: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async login(loginDto: LoginDto): Promise<LoginResponseDto> {
     const { nickname, password } = loginDto;
@@ -15,20 +19,20 @@ export class AuthService {
     const user = await this.prima.user.findUnique({ where: { nickname } });
 
     if (!user) {
-      throw new NotFoundException('Usuário e/ou senha inválidos');
+      throw new UnauthorizedException('Usuário e/ou senha inválidos');
     }
 
     // check if the login information is corret
     const isHashValid = await bcrypt.compare(password, user.password);
 
     if (!isHashValid) {
-      throw new NotFoundException('Usuário e/ou senha inválidos');
+      throw new UnauthorizedException('Usuário e/ou senha inválidos');
     }
 
     delete user.password;
 
     return {
-      token: 'Test',
+      token: this.jwtService.sign({ nickname }),
       user,
     };
   }
--- src/auth/dto/login-response.dto.ts
+++ src/auth/dto/login-response.dto.ts
@@ -4,7 +4,8 @@ import { User } from 'src/user/entities/user.entity';
 export class LoginResponseDto {
   @ApiProperty({
     description: 'JWT gerado pelo login',
-    example: 'TOKEN_GERADO_AUTOMATICAMENTE',
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuaWNrbmFtZSI6ImNoYXJsZXNicmVuZG9uIiwiaWF0IjoxNjU3OTQxNzA5LCJleHAiOjE2NTgwMjgxMDl9.LcLr0CbfkDaCBKSmA7H0ydcXzasoaURukQiyffG8KdA',
   })
   token: string;
 
